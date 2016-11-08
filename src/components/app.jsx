d2d844541953efa108860e975670b13878711991
Eric Cambel <cambelem@appstate.edu>
2016-11-08 12:25:30 -0500

    Added bootstrap to webpack.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,7 +4,14 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-			This is a test with horizon and react.
+			
+			<h1>This is a test <s>with</s> horizon and react.</h1>
+			
+			<div className="btn-group" role="group" aria-label="...">
+				<button type="button" className="btn btn-default">Left</button>
+				<button type="button" className="btn btn-default">Middle</button>
+				<button type="button" className="btn btn-default">Right</button>
+			</div>
 			</div>
 		);
 	}
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './components/app';
 
